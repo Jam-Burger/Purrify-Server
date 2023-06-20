08d57c2fa29c9fc200ea4da2248913e69e502d64
Jam-Burger <jay40793@gmail.com>
2023-06-20 08:57:38 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require("express")
-const app = express()
+const { Server } = require("ws")
 
-app.use(express.json())
+const PORT = process.env.PORT || 3000;
+const server = express()
+    .use(express.json())
+    .listen(PORT, () => {
+        console.log(`server is running on http://localhost:${PORT}`)
+    })
 
-app.get('/', (req, res) => {
-    console.log("this is home page")
-    res.status(200).send("Welcome to Purrify")
-})
-app.post('/login', (req, res) => {
-    const data = req.body
-    console.log("its a post request with data : " + data)
-    res.status(200).json(data)
-})
+// const wss = new Server({ server })
 
-app.listen(3000, () => {
-    console.log("server is running on http://localhost:3000")
-})
+// wss.on('connection', (ws, req) => {
+//     ws.on('message', message => {
+//         var data = message.toString();
+//         console.log(data)
+//         if (data === 'hello') {
+//             console.log('heya!!!')
+//         } else {
+//             ws.send("Are you not saying hi to me 🥺👉👈");
+//         }
+//     })
+// })
+
+// server.get('/', (req, res) => {
+//     console.log("this is home page")
+//     res.status(200).send("Welcome to Purrify")
+// })
+// server.post('/login', (req, res) => {
+//     const data = req.body
+//     console.log("its a post request with data : " + data)
+//     res.status(200).json(data)
+// })
